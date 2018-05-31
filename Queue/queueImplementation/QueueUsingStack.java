6b25c35debf8f13d654fd0f56c0fc01fd67b5a85
avnighael <Avni.Ghael@eRT.com>
2018-05-31 12:46:17 -0400

    Queue implementation using stack

---
+++ Queue/queueImplementation/QueueUsingStack.java
@@ -0,0 +1,101 @@
+package queueImplementation;
+
+import java.util.Stack;
+
+public class QueueUsingStack {
+	
+	private Stack<Integer> s1;
+	
+	/**
+	 * Constructor
+	 */
+	public QueueUsingStack() {
+		s1 = new Stack<Integer>();
+	}
+	
+
+	/**
+	 * Adds the element at the end of the queue
+	 * @param data - it element to be pushed
+	 */
+	public void enqueue(int data) {
+		this.s1.push(data);
+	}
+	
+	/**
+	 * Pops the first element from the queue
+	 * @return - int first element of the queue
+	 */
+	public int dequeue() {
+		int temp;
+		if(this.s1.isEmpty()) {
+			System.out.println("Queue is empty");
+			System.exit(0);
+		}
+		
+		if(this.s1.size() == 1) {
+			return this.s1.pop();
+		}
+		
+		temp = this.s1.pop();
+		int first =  dequeue();
+		this.s1.push(temp);
+		return first;
+	}
+	
+	/**
+	 * Gets the last element of the queue
+	 * @return - int last element
+	 */
+	public int getRear() {
+		if(this.s1.isEmpty()) {
+			System.out.println("Queue is empty");
+			System.exit(0);
+		}
+		return this.s1.peek();
+		
+	}
+	
+	/**
+	 * Gets the front of the queue
+	 * @return - int first element
+	 */
+	public int getFront() {
+		int temp;
+		if(this.s1.isEmpty()) {
+			System.out.println("Queue is empty");
+			System.exit(0);
+		}
+		
+		if(this.s1.size() == 1) {
+			return this.s1.peek();
+		}
+		
+		temp = this.s1.pop();
+		int first =  getFront();
+		this.s1.push(temp);
+		return first;
+
+	}
+	
+
+	public static void main(String[] args) {
+		QueueUsingStack q = new QueueUsingStack();
+		q.enqueue(10);
+		q.enqueue(20);
+		q.enqueue(30);
+		q.enqueue(40);
+		System.out.println(q.getFront());
+		System.out.println(q.getRear());
+		System.out.println(q.dequeue());
+		System.out.println(q.dequeue());
+		System.out.println(q.getRear());
+		System.out.println(q.getFront());
+		System.out.println(q.dequeue());
+		System.out.println(q.dequeue());
+		System.out.println(q.getRear());
+		System.out.println(q.getFront());
+
+	}
+
+}
