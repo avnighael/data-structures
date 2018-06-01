82e1fb54188e5667a06a3c9d89664d1608eff33b
avnighael <Avni.Ghael@eRT.com>
2018-06-01 00:08:57 -0400

    Reverse first k elements of Queue

--- Queue/queueImplementation/QueueUsingStack.java
+++ Queue/queueImplementation/QueueUsingStack.java
@@ -49,12 +49,81 @@ public class QueueUsingStack {
 		return first;
 	}
 	
+	/**
+	 * Reverses the queue recursively
+	 */
 	public void reverse() {
 		if(!this.s1.isEmpty()) {
 			int temp = this.dequeue();
 			reverse();
 			this.enqueue(temp);
 		}
+	}
+	
+	/**
+	 * Reverses the first k elements in the queue
+	 * keeping the remaining elements in the same order
+	 * @param k - int position until the reversal of queue needs to be done
+	 */
+	public void reverseFirstK(int k) {
+		QueueUsingStack q2 = new QueueUsingStack();
+		
+		if(this.isEmpty()) {
+			System.out.println("Queue is empty");
+			return;
+		}
+		
+		if(k > this.size() || k < 0) {
+			System.out.println("k exceeds/ is less than number of elements in the queue");
+			return;
+		}
+		
+		reverseFirstKOnly(q2, k);
+		transfer(this, q2);
+		transfer(q2, this);
+	}
+	
+	/**
+	 * Reverses only the first k elements and return
+	 * @param q2 - the temporary queue to store queue
+	 * @param k - int position until the reversal of queue needs to be done
+	 */
+	public void reverseFirstKOnly(QueueUsingStack q2, int k)  {		
+		if(k > 0) {
+			int temp = this.dequeue();
+			reverseFirstKOnly(q2, k-1);
+			q2.enqueue(temp);
+		}
+
+	}
+	
+	/**
+	 * Transfers all the elements of q1 to q2
+	 * @param q1 - Queue which needs to be transfered
+	 * @param q2 -  Queue which is the destination queue
+	 */
+	public void transfer(QueueUsingStack q1, QueueUsingStack q2) {
+		if(!q1.isEmpty()) {
+			int temp = q1.dequeue();
+			q2.enqueue(temp);
+			transfer(q1, q2);
+		}
+	}
+	
+	/**
+	 * Checks whether the queue is empty or not
+	 * @return - boolean
+	 */
+	public boolean isEmpty() {
+		return this.s1.isEmpty();
+	}
+	
+	/**
+	 * Size of array
+	 * @return - int size of array
+	 */
+	public int size() {
+		return this.s1.size();
 	}
 	
 	/**
@@ -66,8 +135,7 @@ public class QueueUsingStack {
 			System.out.println("Queue is empty");
 			System.exit(0);
 		}
-		return this.s1.peek();
-		
+		return this.s1.peek();		
 	}
 	
 	/**
@@ -99,19 +167,26 @@ public class QueueUsingStack {
 		q.enqueue(20);
 		q.enqueue(30);
 		q.enqueue(40);
-			
-		q.reverse();
+		q.enqueue(50);
+		q.enqueue(60);
 		
+		q.reverseFirstK(4);
 		System.out.println(q.getFront());
 		System.out.println(q.getRear());
 		System.out.println(q.dequeue());
-		System.out.println(q.dequeue());
-		System.out.println(q.getRear());
 		System.out.println(q.getFront());
-		System.out.println(q.dequeue());
-		System.out.println(q.dequeue());
-		System.out.println(q.getRear());
-		System.out.println(q.getFront());
+//		q.reverse();
+//		
+//		System.out.println(q.getFront());
+//		System.out.println(q.getRear());
+//		System.out.println(q.dequeue());
+//		System.out.println(q.dequeue());
+//		System.out.println(q.getRear());
+//		System.out.println(q.getFront());
+//		System.out.println(q.dequeue());
+//		System.out.println(q.dequeue());
+//		System.out.println(q.getRear());
+//		System.out.println(q.getFront());
 
 	}
 
