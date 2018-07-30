1b39f4079089523c8eed2b2b91d5ca1f12fcb45c
avnighael <avi.ghael@gmail.com>
2018-07-30 13:09:18 -0400

    Deletion in BST implemented

--- BinarySearchTree/binarySearchTree/BinarySearchTree.java
+++ BinarySearchTree/binarySearchTree/BinarySearchTree.java
@@ -14,14 +14,27 @@ class Node {
 public class BinarySearchTree {
 	Node root;
 	
+	/**
+	 * Constructor
+	 */
 	BinarySearchTree() {
 		root = null;
 	}
 	
+	/**
+	 * Insert key in BST fashion
+	 * @param key - key to be inserted in BST
+	 */
 	public void insert(int key) {
 		root = insertUtil(root, key);
 	}
 	
+	/**
+	 * Util function to insert key in BST
+	 * @param root - parent of tree/subtree
+	 * @param key - key to be inserted in BST
+	 * @return - root node of BST
+	 */
 	Node insertUtil(Node root, int key) {
 		if(root == null) {
 			root = new Node(key);
@@ -37,10 +50,21 @@ public class BinarySearchTree {
 		return root;
 	}
 	
+	/**
+	 * Search for given key in BST
+	 * @param key - key to be searched
+	 * @return - boolean. true if key is found; false otherwise
+	 */
 	public boolean search(int key) {
 		return searchUtil(root, key);
 	}
 	
+	/**
+	 * Util function to search for given key in BST
+	 * @param root - parent node of tree/subtree
+	 * @param key - key to be searched
+	 * @return - boolean. true if key is found. false otherwise
+	 */
 	public boolean searchUtil(Node root, int key) {
 		if(root == null) {
 			return false;
@@ -58,16 +82,74 @@ public class BinarySearchTree {
 		return false;
 	}
 	
+	/**
+	 * Delete a given key from BST
+	 * @param key - a key to be deleted from BST
+	 */
+	public void delete(int key) {
+		root = deleteUtil(root, key);
+	}
+	
+	/**
+	 * Util to delete the given key from BST
+	 * @param root - parent of tree/subtree
+	 * @param key - key to be deleted from BST
+	 * @return - root node after deleting the given key
+	 */
+	Node deleteUtil(Node root, int key) {
+		if(root == null) {
+			return root;
+		}
+		
+		if(key > root.key) {
+			root.right = deleteUtil(root.right, key);
+		} else if (key < root.key) {
+			root.left = deleteUtil(root.left, key);
+		} else {
+			if(root.left == null) {
+				return root.right;
+			} else if (root.right == null) {
+				return root.left;
+			}
+			
+			root.key = minValue(root.right);
+			
+			root.right = deleteUtil(root.right, root.key);
+		}
+		return root;
+	}
+	
+	/**
+	 * finds the minimum value from the given parent of BST
+	 * @param root - parent of tree/subtree
+	 * @return - minimum value of the BST from given root node
+	 */
+	public int minValue(Node root) {
+		int min = root.key;
+		
+		while(root.left != null) {
+			min = root.left.key;
+			root = root.left;
+		}
+		return min;
+	}
+	
+	/**
+	 * Prints BST by iterating in Inorder fashion.
+	 */
 	public void printInorder() {
 		inorderUtil(root);
 	}
 	
+	/**
+	 * Util function to print BST in Inorder fashion
+	 * @param root - parent node of tree/subtree
+	 */
 	public void inorderUtil(Node root) {
 		if(root != null) {
 			inorderUtil(root.left);
 			System.out.println(root.key);
-			inorderUtil(root.right);
-			
+			inorderUtil(root.right);		
 		}
 	}
 
